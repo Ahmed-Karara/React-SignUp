52f376e5c1d318fab80e7d1a1a69cfb25280a2ba
Ahmed Karara <ahd.66@outlook.com>
2022-12-26 00:49:29 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<App />} />
-			</Routes>
+			<Route path='/' element={<App />} />
 		</BrowserRouter>
 	</React.StrictMode>,
 );
